4f5b06ac92d23dbbe0c717d37bb6f5758120baa1
loaaan <loanvui@live.com>
2024-06-28 13:42:17 -0600

    counter test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter(){
-    let response = await fetch("");
+    let response = await fetch("https://lnutteftkyjlkmbxmhtpkz7hhm0iagab.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
