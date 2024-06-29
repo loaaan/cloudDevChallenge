43cfe927e5e79be8125389f6229e3eeb7d84c2be
loaaan <loanvui@live.com>
2024-06-29 10:59:55 -0600

    still cant make counter work

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter(){
     let response = await fetch("https://lnutteftkyjlkmbxmhtpkz7hhm0iagab.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data.views}`;
+    counter.innerHTML = ` Views: ${data}`;
 }
 
 updateCounter();
